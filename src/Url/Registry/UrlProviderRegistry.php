7c6a47595a76c48cc415d6fd5838b27e81a1c585
johnkrovitch <arnaudfrezet@gmail.com>
2018-11-01 18:04:34 +0100

    Fill missing methods in the url provider registry

--- src/Url/Registry/UrlProviderRegistry.php
+++ src/Url/Registry/UrlProviderRegistry.php
@@ -2,25 +2,57 @@
 
 namespace LAG\SmokerBundle\Url\Registry;
 
+use LAG\SmokerBundle\Exception\Exception;
 use LAG\SmokerBundle\Url\Provider\UrlProviderInterface;
 
 class UrlProviderRegistry
 {
     protected $registry = [];
 
-    public function add(string $name, UrlProviderInterface $provider): void
+    /**
+     * Add a new url provider to the registry. If a provider with the same name is already registered, an exception will
+     * be thrown.
+     *
+     * @param UrlProviderInterface $provider
+     *
+     * @throws Exception
+     */
+    public function add(UrlProviderInterface $provider): void
     {
-        $this->registry[$name] = $provider;
+        if ($this->has($provider->getName())) {
+            throw new Exception('The provider "'.$provider->getName().'" is already registered');
+        }
+        $this->registry[$provider->getName()] = $provider;
     }
 
+    /**
+     * Return an url provider matching the given name. If none exists, an exception will be thrown?
+     *
+     * @param string $name
+     *
+     * @return UrlProviderInterface
+     *
+     * @throws Exception
+     */
     public function get(string $name): UrlProviderInterface
     {
+        if (!$this->has($name)) {
+            throw new Exception('The provider "'.$name.'" is not registered');
+        }
 
+        return $this->registry[$name];
     }
 
+    /**
+     * Return true if a provider with the given name is registered in the registry.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
     public function has(string $name): bool
     {
-
+        return array_key_exists($name, $this->registry);
     }
 
     /**
