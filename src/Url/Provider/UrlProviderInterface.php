3d6b8875a5be2e872230b0752b4fabbad548b299
johnkrovitch <arnaudfrezet@gmail.com>
2018-11-01 19:40:58 +0100

    Add the getName method to the url provider interface

--- src/Url/Provider/UrlProviderInterface.php
+++ src/Url/Provider/UrlProviderInterface.php
@@ -3,10 +3,20 @@
 namespace LAG\SmokerBundle\Url\Provider;
 
 use LAG\SmokerBundle\Url\Collection\UrlCollection;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 interface UrlProviderInterface
 {
-    public function getCollection(): UrlCollection;
+    public function getCollection(array $options = []): UrlCollection;
+
+    public function configureOptions(OptionsResolver $resolver): void;
+
+    /**
+     * Return the provider name.
+     *
+     * @return string
+     */
+    public function getName(): string;
 
     public function getErrorMessages(): array;
 
