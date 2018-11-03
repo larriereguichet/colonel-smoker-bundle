76dc18c21a15bb1bbd906133f1e65b70e20db350
johnkrovitch <arnaudfrezet@gmail.com>
2018-11-03 21:41:41 +0100

    Change methods signature in the success response handler

--- src/Response/Handler/ResponseHandlerInterface.php
+++ src/Response/Handler/ResponseHandlerInterface.php
@@ -3,11 +3,19 @@
 namespace LAG\SmokerBundle\Response\Handler;
 
 use Goutte\Client;
+use LAG\SmokerBundle\Exception\Exception;
 use Symfony\Component\DomCrawler\Crawler;
 
 interface ResponseHandlerInterface
 {
     public function supports(string $routeName): bool;
 
-    public function handle(string $routeName, Crawler $crawler, Client $client);
+    /**
+     * @param string  $routeName
+     * @param Crawler $crawler
+     * @param Client  $client
+     *
+     * @throws Exception
+     */
+    public function handle(string $routeName, Crawler $crawler, Client $client): void;
 }
--- src/Response/Handler/SuccessResponseHandler.php
+++ src/Response/Handler/SuccessResponseHandler.php
@@ -9,18 +9,23 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SuccessResponseHandler implements ResponseHandlerInterface
 {
+    /**
+     * @inheritdoc
+     */
     public function supports(string $routeName): bool
     {
         return true;
     }
 
-    public function handle(string $routeName, Crawler $crawler, Client $client)
+    /**
+     * @inheritdoc
+     */
+    public function handle(string $routeName, Crawler $crawler, Client $client): void
     {
         if (null === $client->getResponse()) {
             throw new Exception('The client has no response. It should make a request before handle a response');
         }
         $responseCode = $client->getResponse()->getStatus();
-        //dump($client->getResponse());
 
         if (Response::HTTP_OK !== $client->getResponse()->getStatus()) {
             throw new Exception('Excepted code 200, got '.$responseCode.' for route '.$routeName);
